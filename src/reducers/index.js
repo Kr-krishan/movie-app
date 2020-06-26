12a7634f9db77f91e0b4d0076d28556a2a6af1d6
Kr-Krishan <krishanilam@gmail.com>
2020-06-26 20:25:06 +0530

    changed the state from array to obj

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,16 @@
 import {ADD_MOVIES} from '../actions/index';
 
-export default function movies(state=[],action){   //if nothing is passed in state it will take this default empty array
+const initialMoviesState={
+    list:[],
+    favourites:[]
+}
+
+export default function movies(state=initialMoviesState,action){   //if nothing is passed in state it will take this default empty array
     if(action.type === ADD_MOVIES){
-        return action.movies;
+        return {
+            ...state,
+            list:action.movies
+        }
     }
     return state;
 }
