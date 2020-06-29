43ce786ddc5912743265d3186c353f256c166215
Kr-Krishan <krishanilam@gmail.com>
2020-06-29 13:50:52 +0530

    added navbar basic search result

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,10 @@
-// {
-//     type:'ADD_MOVIES'
-// }
-// {
-//     type:'DECREASE_COUNT'
-// }
-
 
 // action types
 export const ADD_MOVIES='ADD_MOVIES';
 export const ADD_FAVOURITES='ADD_FAVOURITES';
 export const REMOVE_FAVOURITES='REMOVE_FAVOURITES';
 export const SET_SHOW_FAVOURITES='SET_SHOW_FAVOURITES';
+export const ADD_MOVIE_TO_LIST='ADD_MOVIE_TO_LIST';
 
 
 //action creators
@@ -39,5 +33,12 @@ export function setShowFavourites(val){
     return {
         type:SET_SHOW_FAVOURITES,
         val
+    }
+};
+
+export function addMovieToList(movie){
+    return {
+        type:ADD_MOVIE_TO_LIST,
+        movie
     }
 };
