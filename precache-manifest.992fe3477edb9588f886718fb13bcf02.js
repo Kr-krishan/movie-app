2e1905c4385600be1d5e27adcce01a88d1480b32
Kr-Krishan <krishanilam@gmail.com>
2020-08-02 12:26:46 +0530

    Updates

---
+++ precache-manifest.992fe3477edb9588f886718fb13bcf02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d3753eb5cd341ffab26142a9f07612",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "845f2938d59ece39c2be",
+    "url": "/movies-app/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "158623f8e8170566d64c",
+    "url": "/movies-app/static/js/2.4f6d887e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-app/static/js/2.4f6d887e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845f2938d59ece39c2be",
+    "url": "/movies-app/static/js/main.c0741d68.chunk.js"
+  },
+  {
+    "revision": "0453a1cdb7243338383b",
+    "url": "/movies-app/static/js/runtime-main.5950ab57.js"
+  }
+]);
